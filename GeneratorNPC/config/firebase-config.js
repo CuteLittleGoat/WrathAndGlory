77afb49aec3ae8df481cfecdc0f813a0fce9aed4
CuteLittleGoat <taryf@o2.pl>
2026-02-05 16:59:24 +0100

    Add files via upload

---
+++ GeneratorNPC/config/firebase-config.js
@@ -0,0 +1,12 @@
+// Skopiuj ten plik jako: config/firebase-config.js (bez ".template")
+// Następnie wklej swój firebaseConfig z:
+// Firebase Console → Project settings → Your apps (Web) → Firebase SDK snippet (Config)
+
+window.firebaseConfig = {
+  apiKey: "AIzaSyA4m8ZkI-L9MYgKcbAtt8gJaTtr2TlCN5s",
+  authDomain: "audiorpg-2eb6f.firebaseapp.com",
+  projectId: "audiorpg-2eb6f",
+  storageBucket: "audiorpg-2eb6f.firebasestorage.app",
+  messagingSenderId: "848367463263",
+  appId: "1:848367463263:web:500bdf5c394ac786e2e4ea"
+};
